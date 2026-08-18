49aa6d0412d8ae75378c9e23cef3389baab536cf
大痴小乙 <fxbin123@gmail.com>
2026-08-18 20:29:28 +0800

    refactor: use relative config import in database core

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,6 +1,6 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
-import { getDatabaseUrl } from "@/config/env";
+import { getDatabaseUrl } from "../config/env";
 import * as schema from "./schema";
 
 type SqlClient = ReturnType<typeof postgres>;
